71b96f73bffa2d4123edfe19c935e8a3463d8990
SleamanAbdelhadi <sabdelhadi@appdev.yearup.org>
2023-11-03 16:01:23 -0400

    uploading to my repository

---
+++ the-pluralsight-hotel/src/main/java/pluralsight/hotel/Room.java
@@ -0,0 +1,12 @@
+package pluralsight.hotel;
+
+public class Room {
+    int number;
+    boolean available;
+    boolean isClean;
+    RoomType roomType;
+
+}
+/*-----Getters-----*/
+new*
+int getNumberOf
